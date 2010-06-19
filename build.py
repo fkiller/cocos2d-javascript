30a3d26894eb595f428bf9aac8d89e8c33482d71
Ryan Williams <ryan@wigg.ly>
2010-06-19 20:21:54 +1200

    Added support for plain text resources

--- build.py
+++ build.py
@@ -2,8 +2,12 @@
 
 from optparse import OptionParser
 from cStringIO import StringIO
-import re, os, base64
+import re, os, base64, mimetypes, json
 
+mimetypes.add_type('application/xml', '.tmx')
+mimetypes.add_type('application/xml', '.tsx')
+
+TEXT_MIMETYPES = 'application/xml text/plain text/json application/json text/javascript application/javascript text/html'.split(' ')
 
 SOURCE_CODE_TEMPLATE = '''
 window.__source_files__["%s"] = (function() {
@@ -19,8 +23,11 @@ window.__source_files__["%s"] = (function() {
 });
 '''
 
-RESOURCE_TEMPLATE = '''
+BINARY_RESOURCE_TEMPLATE = '''
 window.__resources__["%s"] = "data:%s;base64,%s";
+'''
+TEXT_RESOURCE_TEMPLATE = '''
+window.__resources__["%s"] = %s;
 '''
 
 class Builder(object):
@@ -32,14 +39,22 @@ class Builder(object):
     def build(self, source, output=None):
         code = open('system.js').read()
         for root, dirs, files in os.walk(source):
-            if re.match(r".*/resources$", root):
+            if re.match(r".*/resources($|/)", root):
                 for f in files:
+                    if f[0] == '.':
+                        continue
                     path = os.path.join(root, f)
                     resources_name = './%s' % path[len(source) +1:] # Remove source path prefix
-                    mimetype = 'image/png'
+                    mimetype = mimetypes.guess_type(path)[0]
                     data = StringIO()
-                    base64.encode(open(path), data)
-                    code += RESOURCE_TEMPLATE % (resources_name, mimetype, data.getvalue().replace('\n', ''))
+
+                    is_binary = (mimetype not in TEXT_MIMETYPES)
+
+                    if is_binary:
+                        base64.encode(open(path), data)
+                        code += BINARY_RESOURCE_TEMPLATE % (resources_name, mimetype, data.getvalue().replace('\n', ''))
+                    else:
+                        code += TEXT_RESOURCE_TEMPLATE % (resources_name, json.dumps(open(path).read()))
             else:
                 for f in files:
                     if f[-3:] != '.js':
@@ -122,80 +137,18 @@ class Builder(object):
         return code
 
 
-
-    def parse_loads(self, code):
-        """
-        Parses @load tags.
-        A @load imports an app and wraps the whole thing in a function/namespace.
-        """
-        def replace_load(matches):
-            load_filename = '../apps/' + matches.group('filename') + '/main.js'
-            print "Loading app: %s" % load_filename
-
-            # If already loadd, don't include it again
-            if load_filename in self.loaded_files:
-                return ''
-
-            self.loaded_files.append(load_filename)
-
-            code = '''
-(function() {
-%s
-})()
-            ''' % self.parse(load_filename)
-            return code
-
-        load_re = re.compile('''@load\s+(?P<q>"|')(?P<filename>.+)(?P=q)\s*;?''')
-        return load_re.sub(replace_load, code)
-
-    def parse_requires(self, code):
-        """
-        Parses @require tags.
-        A @require is only included once. If you attempt to @require a file
-        more than once, then subsequent attempts will be ignored.
-        """
-        def replace_require(matches):
-            require_filename = os.getcwd() + '/' + matches.group('filename')
-            print "Requiring: %s" % require_filename
-
-            # If already required, don't include it again
-            if require_filename in self.required_files:
-                return ''
-
-            self.required_files.append(require_filename)
-
-            return self.parse(require_filename)
-
-        require_re = re.compile('''@require\s+(?P<q>"|')(?P<filename>.+)(?P=q)\s*;?''')
-        return require_re.sub(replace_require, code)
-
-    def parse_imports(self, code):
-        """
-        Parses @import tags.
-        @import works a lot like @require but will import a file every time it's used.
-        It is possible to cause infinite import loops using this.
-        """
-        def replace_import(matches):
-            import_filename = matches.group('filename')
-            return self.parse(import_filename)
-
-        import_re = re.compile('''@import\s+(?P<q>"|')(?P<filename>.+)(?P=q)\s*;?''')
-        return import_re.sub(replace_import, code)
-
-
-
 def main():
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="output",
                       help="write code to FILE", metavar="FILE")
 
     parser.add_option("-s", "--source", dest="input",
-                      help="build source code in SRC. Default is main.js", metavar="SRC")
+                      help="build source code in SRC. Default is src", metavar="SRC")
 
     (options, args) = parser.parse_args()
 
     builder = Builder()
-    print builder.build(options.input or 'main.js', options.output)
+    print builder.build(options.input or 'src', options.output)
 
 if __name__ == "__main__":
     main()
