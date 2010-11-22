5651b99357b70ef8dcb3bf33f65803c5d873b609
Ryan Williams <ryan@wigg.ly>
2010-11-22 19:56:47 +1300

    Fixed rendering of TMX maps with tile IDs > 256

--- src/libs/base64.js
+++ src/libs/base64.js
@@ -16,10 +16,11 @@ var base64 = {
 
         var dec = JXG.Util.Base64.decode(input),
             ar = [], i, j, len;
+
         for (i = 0, len = dec.length/bytes; i < len; i++){
             ar[i] = 0;
             for (j = bytes-1; j >= 0; --j){
-                ar[i] += dec.charCodeAt((i *bytes) +j) << j;
+                ar[i] += dec.charCodeAt((i *bytes) +j) << (j *8);
             }
         }
         return ar;
