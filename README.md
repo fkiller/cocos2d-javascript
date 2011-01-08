c2456ec855c5b75983a44f3d57fc9bd8e630f7f8
Ryan Williams <ryan@wigg.ly>
2011-01-09 12:58:56 +1300

    Added script for creating new projects

--- README.md
+++ README.md
@@ -5,22 +5,37 @@ This is in the early stages of development and could break backwards compatibili
 * You can find me on Twitter: @cocos2djs
 * Email: <ryan@cocos2d-javascript.org>
 * Website: <http://cocos2d-javascript.org/>
+* Documentation: <http://cocos2d-javascript.org/documentation>
 * Forum: <http://cocos2d-javascript.org/forum>
 
-Example Code
-------------
+Creating a new project
+----------------------
 
-If you want a very basic example and a good starting point then you should checkout
-<https://github.com/RyanWilliams/cocos2d-helloworld>
+To create a new project you first need to get a copy of cocos2d-javascript. Grab the latest using git
+
+    git clone git://github.com/RyanWilliams/cocos2d-javascript.git
+
+To create your initial project simply navigate to the cocos2d-javascript directory and run
+
+    ./cocos new ~/Projects/MyApp
+
+This will create a new directory at that path with everything you need to
+get started. It will also add cocos2d-javascript as a git submodule. If you
+prefer to just copy the cocos2d-javascript code instead of using a git
+submodule then use
+
+    ./cocos new ~/Projects/MyApp -g
 
 Getting things running
 ----------------------
 
-To get things up and running simply run the development web server
+To get your project running simply navigate to the newly created directory run
+the development web server
 
-    $ ./cocos server
+    ./cocos server
 
-And visit <http://localhost:4000/>. You will see a selection of tests you can run.
+And visit <http://localhost:4000/>. There you will see a simple application
+outputting your application name.
 
 Developing
 ----------
@@ -31,17 +46,15 @@ including images, sound files, map files, etc.
 
 The entry point for the code is path defined as "main.js" inside the in "make.js" file.
 
-In the public/index.html you will see &lt;script src="cocos2d.js"&gt; tag to include the code.
+In the public/index.html you will see &lt;script src="appname.js"&gt; tag to include the code.
 
-The development web server will compile your code each time it is
-requested. This makes development a lot easier.
+The web server will compile your code each time it is requested. This makes
+development a lot easier.
 
 Run ./cocos server -h for help.
 
-More documentation is coming when the framework is in a more usable state.
-
-Compiling your app
-------------------
+Compiling your application
+--------------------------
 
 To compile your code you run ./cocos make. Which reads the make.js file
 to work out what you want to build.
@@ -66,9 +79,9 @@ Download JsDoc 2.3 (or 2.4) from <http://code.google.com/p/jsdoc-toolkit/>.
 
 Copy that to /usr/local/jsdoc-toolkit or wherever you like and then run:
     
-    java -jar /usr/local/jsdoc-toolkit/jsrun.jar /usr/local/jsdoc-toolkit/app/run.js -t=/usr/local/jsdoc-toolkit/templates/jsdoc -d=./jsdocs/ -r 10 src/
+    JSDOC_HOME=/usr/local/jsdoc-toolkit ./jsdoc
 
-The documentation will appear in the jsdocs directory.
+The documentation will appear in the 'docs' directory.
 
 © 2010 Ryan Williams <ryan@cocos2d-javascript.org>
 
---
+++ scripts/new.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import subprocess
+import sys, re, os, base64, mimetypes, codecs, shutil
+
+INDEX_TEMPLATE = '''
+<!doctype html> 
+<html> 
+    <head> 
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+        <script src="{script_name}" type="text/javascript"></script> 
+        <title>{project_name}</title> 
+        <style type="text/css" media="screen"> 
+            body {{
+                font-family: Helvetica, Arial, sans-serif;
+                font-size: 10pt;
+            }}
+            #cocos2d-app {{
+                border: 1px solid #000;
+                width: 480px;
+                height: 320px;
+                margin: 0 20px;
+            }}
+        </style> 
+    </head> 
+    <body> 
+        <h1>{project_name}</h1> 
+       <div id="cocos2d-app"></div> 
+    </body> 
+</html> 
+'''
+
+MAKE_JS_TEMPLATE = '''
+{{
+    "output":     "public/{script_name}",
+    "extensions": ["js", "gif", "jpeg", "jpg", "png", "tmx", "tsx", "plist"],
+    "main_module": "main",
+
+    "paths": {{
+        "cocos2d/src" : "/",
+        "src" : "/"
+    }}
+}}
+'''
+
+MAIN_JS_TEMPLATE = '''
+// Import the cocos2d module
+var cocos = require('cocos2d'),
+// Import the geometry module
+    geo = require('geometry');
+
+// Create a new layer
+var {class_name} = cocos.nodes.Layer.extend({{
+    init: function() {{
+        // You must always call the super class version of init
+        @super;
+
+        // Get size of canvas
+        var s = cocos.Director.get('sharedDirector').get('winSize');
+
+        // Create label
+        var label = cocos.nodes.Label.create({{string: '{project_name}', fontName: 'Arial', fontSize: 76}});
+
+        // Add label to layer
+        this.addChild({{child: label, z:1}});
+
+        // Position the label in the centre of the view
+        label.set('position', geo.ccp(s.width / 2, s.height / 2));
+    }}
+}});
+
+// Initialise everything
+
+// Get director
+var director = cocos.Director.get('sharedDirector');
+
+// Attach director to our <div> element
+director.attachInView(document.getElementById('cocos2d-app'));
+
+// Create a scene
+var scene = cocos.nodes.Scene.create();
+
+// Add our layer to the scene
+scene.addChild({{child: {class_name}.create()}});
+
+// Run the scene
+director.runWithScene(scene);
+'''
+
+
+def main():
+    def removeNonAscii(s):
+        return "".join(i for i in s if ord(i) < 128)
+
+    parser = OptionParser(usage="Usage: cocos new APP_PATH")
+    parser.add_option("-g", "--no-git", action="store_true", dest="nogit")
+
+    (options, args) = parser.parse_args()
+
+    use_git = not options.nogit
+    project_path = os.path.abspath(args[0])
+    project_name = os.path.basename(project_path)
+    script_name = removeNonAscii(project_name.lower().replace(' ', '_')) +'.js'
+    class_name = re.sub('(?:^| )(.)', lambda m: m.group(1).upper(), removeNonAscii(project_name.lower()))
+
+    paths = (
+        'src',
+        'src/resources',
+        'public',
+    )
+    
+    # Create initial folders
+    print "Creating cocos2d-javascript project in: %s" % project_path
+    for p in paths:
+        full_path = os.path.join(project_path, p)
+        if not os.path.exists(full_path):
+            print "Creating new directory : %s" % full_path
+            os.makedirs(full_path)
+
+    subs = {'project_name': project_name, 'script_name': script_name, 'class_name': class_name}
+
+    # Write out public/index.html
+    index = open(os.path.join(project_path, 'public/index.html'), 'w')
+    index.write(INDEX_TEMPLATE.format(**subs))
+    index.close()
+
+    # Write out make.js
+    make_js = open(os.path.join(project_path, 'make.js'), 'w')
+    make_js.write(MAKE_JS_TEMPLATE.format(**subs))
+    make_js.close()
+
+    # Write out src/main.js
+    main_js = open(os.path.join(project_path, 'src/main.js'), 'w')
+    main_js.write(MAIN_JS_TEMPLATE.format(**subs))
+    main_js.close()
+
+
+    # Create cocos2d code
+    if use_git:
+        print "Creating cocos2d git submodule"
+        os.chdir(project_path)
+        subprocess.call(['git', 'init'])
+        subprocess.call(['git', 'submodule', 'add', 'git://github.com/RyanWilliams/cocos2d-javascript.git', 'cocos2d'])
+    else:
+        print "Copying cocos2d"
+        cocos_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        shutil.copytree(cocos_path, os.path.join(project_path, 'cocos2d'))
+        os.chdir(project_path)
+
+    shutil.copy2('cocos2d/cocos', './cocos')
+
+
+    
+
+if __name__ == "__main__":
+    main()
