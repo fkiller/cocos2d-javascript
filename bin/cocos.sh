046ba75965eadb4009a68667a0132444c91ddc83
Ryan Williams <ryan@wigg.ly>
2011-02-08 18:34:43 +1300

    Fixed cocos.sh script so it can run from any path

--- bin/cocos.sh
+++ bin/cocos.sh
@@ -1,25 +1,27 @@
 #!/bin/sh -e
 # lets check if we have the submodules initialized
 
+DIR=`dirname $0`
+
 case `uname -a` in
 Linux*x86_64*)  echo "Linux 64 bit"   
-	support/node-builds/lin64/node bin/cocos.js "$@" 
+    "$DIR/../support/node-builds/lin64/node" "$DIR/cocos.js" "$@" 
 	;;
 
 Linux*i686*)  echo "Linux 32 bit"   
-	support/node-builds/lin32/node bin/cocos.js "$@"
+	"$DIR/../support/node-builds/lin32/node" "$DIR/cocos.js" "$@"
 	;;
     
 Darwin*)  echo  "OSX"
-    support/node-builds/osx64/node bin/cocos.js "$@"
+    "$DIR/../support/node-builds/osx64/node" "$DIR/cocos.js" "$@"
     ;;
 
 CYGWIN*)  echo  "Cygwin"
-    support/node-builds/win32/node.exe bin/cocos.js "$@"
+    "$DIR/../support/node-builds/win32/node.exe" "$DIR/cocos.js" "$@"
     ;;
 
 MING*)  echo  "MingW"
-    support/node-builds/win32/node.exe bin/cocos.js "$@"
+    "$DIR/../support/node-builds/win32/node.exe" "$DIR/cocos.js" "$@"
     ;;    
 
 *) echo "Unknown OS"
