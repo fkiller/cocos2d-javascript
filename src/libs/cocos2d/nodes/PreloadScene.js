cb45171a83511eab3e07ee64a24aab886288919c
Ryan Williams <ryan@wigg.ly>
2011-05-10 23:49:59 +1200

    Fixed whitespace in code

--- src/libs/cocos2d/nodes/PreloadScene.js
+++ src/libs/cocos2d/nodes/PreloadScene.js
@@ -8,68 +8,68 @@ var Scene       = require('./Scene').Scene,
     ProgressBar = require('./ProgressBar').ProgressBar,
     Preloader   = require('../Preloader').Preloader,
     geo         = require('geometry'),
-	events      = require('events');
+    events      = require('events');
 
 var PreloadScene = Scene.extend(/** @lends cocos.nodes.PreloadScene# */{
-	progressBar: null,
-	label: null,
+    progressBar: null,
+    label: null,
 
-	/**
-	 * @memberOf cocos.nodes
-	 * @extends cocos.nodes.Scene
-	 * @constructs
-	 */
-	init: function (opts) {
-		PreloadScene.superclass.init.call(this, opts);
+    /**
+     * @memberOf cocos.nodes
+     * @extends cocos.nodes.Scene
+     * @constructs
+     */
+    init: function (opts) {
+        PreloadScene.superclass.init.call(this, opts);
 
-		var pb = ProgressBar.create();
-		this.set('progressBar', pb);
-		this.addChild({child: pb});
+        var pb = ProgressBar.create();
+        this.set('progressBar', pb);
+        this.addChild({child: pb});
 
-		var label = Label.create({
-			fontSize: 14,
-			fontName: 'Helvetica',
-			fontColor: '#ffffff',
-			string: 'Please wait...'
-		});
-		this.set('label', label);
-		this.addChild({child: label});
-	},
+        var label = Label.create({
+            fontSize: 14,
+            fontName: 'Helvetica',
+            fontColor: '#ffffff',
+            string: 'Please wait...'
+        });
+        this.set('label', label);
+        this.addChild({child: label});
+    },
 
-	onEnter: function () {
-		PreloadScene.superclass.onEnter.call(this);
+    onEnter: function () {
+        PreloadScene.superclass.onEnter.call(this);
         var size = Director.get('sharedDirector').get('winSize'),
-		    preloader = Preloader.create(),
-			progressBar = this.get('progressBar'),
-			label = this.get('label');
+            preloader = Preloader.create(),
+            progressBar = this.get('progressBar'),
+            label = this.get('label');
 
-		// Position and size label and progress bar
-		label.set('position', new geo.Point(size.width / 2, (size.height / 2) + 32));
-		progressBar.set('position', new geo.Point(size.width / 2, size.height / 2));
-		progressBar.set('contentSize', new geo.Size(size.width / 2, 32));
-
-
+        // Position and size label and progress bar
+        label.set('position', new geo.Point(size.width / 2, (size.height / 2) + 32));
+        progressBar.set('position', new geo.Point(size.width / 2, size.height / 2));
+        progressBar.set('contentSize', new geo.Size(size.width / 2, 32));
 
 
 
-		progressBar.bindTo('maxValue', preloader, 'count');
-		progressBar.bindTo('value',    preloader, 'loaded');
 
-		var self = this;
-		events.addListener(preloader, 'load', function (uri, preloader) {
-			var loaded = preloader.get('loaded'),
-				count = preloader.get('count');
-			console.log("Loaded: %d%% -- %d of %d -- %s", (loaded / count) * 100, loaded, count, uri);
 
-			events.trigger(self, 'load', uri, preloader);
-		});
+        progressBar.bindTo('maxValue', preloader, 'count');
+        progressBar.bindTo('value',    preloader, 'loaded');
 
-		events.addListener(preloader, 'complete', function (preloader) {
-			events.trigger(self, 'complete', preloader);
-		});
+        var self = this;
+        events.addListener(preloader, 'load', function (uri, preloader) {
+            var loaded = preloader.get('loaded'),
+                count = preloader.get('count');
+            console.log("Loaded: %d%% -- %d of %d -- %s", (loaded / count) * 100, loaded, count, uri);
 
-		preloader.load();
-	}
+            events.trigger(self, 'load', uri, preloader);
+        });
+
+        events.addListener(preloader, 'complete', function (preloader) {
+            events.trigger(self, 'complete', preloader);
+        });
+
+        preloader.load();
+    }
 });
 
 exports.PreloadScene = PreloadScene;
--- src/libs/cocos2d/nodes/ProgressBar.js
+++ src/libs/cocos2d/nodes/ProgressBar.js
@@ -8,32 +8,32 @@ var Node   = require('./Node').Node,
     events = require('events');
 
 var ProgressBar = Node.extend(/** @lends cocos.nodes.ProgressBar# */{
-	maxValue: 100,
-	value: 0,
+    maxValue: 100,
+    value: 0,
 
-	/**
-	 * @memberOf cocos.nodes
-	 * @extends cocos.nodes.Node
-	 * @constructs
-	 */
-	init: function (opts) {
-		ProgressBar.superclass.init.call(this, opts);
-	},
+    /**
+     * @memberOf cocos.nodes
+     * @extends cocos.nodes.Node
+     * @constructs
+     */
+    init: function (opts) {
+        ProgressBar.superclass.init.call(this, opts);
+    },
 
-	draw: function (ctx) {
-		var size = this.get('contentSize'),
-			value = this.get('value'),
-			maxValue = this.get('maxValue');
+    draw: function (ctx) {
+        var size = this.get('contentSize'),
+            value = this.get('value'),
+            maxValue = this.get('maxValue');
 
 
-		// Outline
-		ctx.fillStyle = '#ffffff';
-		ctx.fillRect(0, 0, size.width, size.height);
-		ctx.fillStyle = '#000000';
-		ctx.fillRect(3, 3, size.width - 6, size.height - 6);
-		ctx.fillStyle = '#ffffff';
-		ctx.fillRect(6, 6, (size.width - 12) * (value / maxValue), size.height - 12);
-	}
+        // Outline
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(0, 0, size.width, size.height);
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(3, 3, size.width - 6, size.height - 6);
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(6, 6, (size.width - 12) * (value / maxValue), size.height - 12);
+    }
 });
 
 exports.ProgressBar = ProgressBar;
